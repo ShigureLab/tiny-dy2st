51a5c60f4c2ff22e42e02a13d1d56f42e84957ea
SigureMo <sigure.qaq@gmail.com>
2023-04-17 16:27:06 +0800

    💡 chore: add some comments

--- tiny_dy2st/dy2st/dy2st.py
+++ tiny_dy2st/dy2st/dy2st.py
@@ -20,7 +20,9 @@ CACHE: dict[str, tuple[Node, list[Variable], Variable]] = {}
 
 def dy2st(dyfunc: Callable[Input, Output]) -> Callable[Input, Output]:
     def wrapper(*args: Tensor):
+        # Step 1: 转写期
         dysrc = inspect.getsource(dyfunc)
+        # 利用 Cache 避免重复组网
         if dysrc in CACHE:
             graph, args_vars, out_var = CACHE[dysrc]
             args_values = [arg.data for arg in args]
@@ -29,6 +31,7 @@ def dy2st(dyfunc: Callable[Input, Output]) -> Callable[Input, Output]:
                 for arg_var, arg_value in zip(args_vars, args_values)
             }
         else:
+            # 源码转写
             stsrc = dysrc2stsrc(dysrc)
 
             if TINY_DY2ST_DEBUG is not None and TINY_DY2ST_DEBUG.lower() in [
@@ -38,6 +41,7 @@ def dy2st(dyfunc: Callable[Input, Output]) -> Callable[Input, Output]:
             ]:
                 print("Transformed source code:")
                 print(stsrc)
+            # 执行以获取静态函数
             func_name = dyfunc.__name__
             exec_globals = dict(globals())
             exec_globals.update(
@@ -55,19 +59,26 @@ stfunc = {func_name}
             )
             stfunc = exec_globals["stfunc"]
 
+            # Step 2: 组网期
             args_values = [arg.data for arg in args]
+            # Tensor -> Variable
             args_vars = [
                 create_variable(arg.name, arg.dtype, arg.shape) for arg in args
             ]
 
+            # 构造输入
             inputs_dict = {
                 arg_var.name: arg_value
                 for arg_var, arg_value in zip(args_vars, args_values)
             }
 
+            # 传入 Variable 进行组网
             out_var = stfunc(*args_vars)
             graph = GLOBAL_GRAPH[out_var.name]
             CACHE[dysrc] = graph, args_vars, out_var
+
+        # Step 3: 执行期
+        # 利用动态图 run_static_graph API 来运行静态图
         out = run_static_graph(graph, inputs_dict, [out_var.name])
         return create_tensor(
             unique_name(), out_var.dtype, out_var.shape, out[out_var.name]
